4e71a4be6c0ee22e8d76a265b13588e6a6d26d7c
theplayerofgames <theplayerofgames@protonmail.com>
2018-05-25 15:04:04 +0100

    Update language of contract

--- migrations/2_deploy_token.js
+++ migrations/2_deploy_token.js
@@ -1,8 +1,8 @@
-const Crowdsale = artifacts.require("./Crowdsale.sol")
+const Fundraise = artifacts.require("./Fundraise.sol")
 
 module.exports = function(deployer, network, accounts) {
   const startTime = web3.eth.getBlock(web3.eth.blockNumber).timestamp + 240 // seconds
   const endTime = startTime + (86400 * 241); // End on 31st December 2018
   const rate = new web3.BigNumber(0.0227272727272727); // 1/44 (1 ETC should incept 44 NCC)
-  deployer.deploy(Crowdsale, startTime, endTime, rate, { gas: 2000000 })
+  deployer.deploy(Fundraise, startTime, endTime, rate, { gas: 2000000 })
 };
